9a3c8a1196b7b8ecfcc313f4dbc9f136abe0ab60
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-11-15 11:16:33 +0100

    Created DI IosModule

---
+++ shared/src/iosMain/kotlin/com/ag_apps/restaurants/di/IosModule.kt
@@ -0,0 +1,21 @@
+package com.ag_apps.restaurants.di
+
+import com.ag_apps.restaurants.data.LocalRestaurantDataSource
+import com.ag_apps.restaurants.domain.LocationService
+import com.ag_apps.restaurants.domain.RestaurantDataSource
+import com.ag_apps.restaurants.domain.usecase.GetRestaurantsUseCase
+
+/**
+ * @author Ahmed Guedmioui
+ */
+class IosModule {
+    private val locationService by lazy { LocationService() }
+
+    private val restaurantDataSource: RestaurantDataSource by lazy {
+        LocalRestaurantDataSource(locationService)
+    }
+
+    val getRestaurantsUseCase: GetRestaurantsUseCase by lazy {
+        GetRestaurantsUseCase(restaurantDataSource)
+    }
+}
